261a4b5d8150574e8b947e4d76a73ad26b5b8720
DatPhamABC <datpham5810@gmail.com>
2021-09-09 13:34:11 +0700

    v.1.1 Connect Mongodb

--- spiderCrawler/multiSpider.py
+++ spiderCrawler/multiSpider.py
@@ -2,12 +2,15 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from spiderCrawler.spiders.LinkCrawl import CrawlSpider
 
-process = CrawlerProcess(settings={
-    'FEED_URI': 'link.csv',
-    'FEED_FORMAT': 'csv'
-})
+process = CrawlerProcess(
+#     settings={
+#     'FEED_URI': 'link.csv',
+#     'FEED_FORMAT': 'csv'
+# }
+)
 
-for i in range(1, 10):
-    process.crawl(CrawlSpider, i*10)
+for i in range(0, 20):
+    pageCount = 50
+    process.crawl(CrawlSpider, i*pageCount+1)
 
 process.start()
--- spiderCrawler/spiderCrawler/spiders/LinkCrawl.py
+++ spiderCrawler/spiderCrawler/spiders/LinkCrawl.py
@@ -1,18 +1,27 @@
 import scrapy
 import re
+import pymongo
 
 
 class CrawlSpider(scrapy.Spider):
     name = 'linkcrawl'
+
+    # CLOSESPIDER_PAGECOUNT must equal the number of pageCount in multiSpider.py
     custom_settings = {'FEED_EXPORT_ENCODING': 'utf-8',
-                       'CLOSESPIDER_PAGECOUNT': 30}
+                       'CLOSESPIDER_PAGECOUNT': 50}
 
     def __init__(self, category, *args, **kwargs):
         super(CrawlSpider, self).__init__(*args, **kwargs)
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        db = self.conn['scrapedLink']
+        self.collection = db['link']
         self.start_urls = [f'https://vnexpress.net/thoi-su-p{category}']
 
     def parse(self, response):
-        const_str = ['covid', 'F0', 'F1', 'dịch', 'giãn']
+        const_str = ['covid', 'F0', 'F1', 'dịch', 'giãn', 'xét nghiệm', 'tiêm', 'giấy đi đường']
         const_str = [x.lower().encode() for x in const_str]
 
         for article in response.css('#automation_TV0 div.width_common.list-news-subfolder'):
@@ -25,14 +34,13 @@ class CrawlSpider(scrapy.Spider):
                 if not self.check(desc, const_str):
                     continue
                 else:
-                    link = article.css('a').attrib['href']
-                    yield {
-                        'link': link
+                    link = {
+                        'link': article.css('a').attrib['href']
                     }
+                    self.collection.insert(dict(link))
 
         if response.css('#automation_TV1 div.width_common.list-news-subfolder'):
-            auto1 = response.css('#automation_TV1 div.width_common.list-news-subfolder')
-            for article in auto1:
+            for article in response.css('#automation_TV1 div.width_common.list-news-subfolder'):
                 title = article.css('h3.title-news a::text').get()
                 title = title.replace('\n', '')
                 if not self.check(title, const_str):
@@ -42,10 +50,10 @@ class CrawlSpider(scrapy.Spider):
                     if not self.check(desc, const_str):
                         continue
                     else:
-                        link = article.css('a').attrib['href']
-                        yield {
-                            'link': link
+                        link = {
+                            'link': article.css('a').attrib['href']
                         }
+                        self.collection.insert(dict(link))
 
         next_page = response.css('a.btn-page.next-page ::attr(href)').get()
         if next_page is not None:
