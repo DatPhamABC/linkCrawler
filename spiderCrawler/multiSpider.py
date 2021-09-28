cdc7eef9f28394672d025119d6fe2a2a7243d046
DatPhamABC <datpham5810@gmail.com>
2021-09-28 11:11:48 +0700

    v.1.4 Minor change

--- spiderCrawler/multiSpider.py
+++ spiderCrawler/multiSpider.py
@@ -3,19 +3,23 @@ from spiderCrawler.spiders.LinkCrawl import CrawlSpider
 from scrapy.utils.project import get_project_settings
 
 
-process = CrawlerProcess()
-totalPageCount = 15
-setting = get_project_settings()
-spiderPageCount = setting.get('CLOSESPIDER_PAGECOUNT')
+class mulSpider:
+    def __init__(self, totalPageCount):
+        self.totalPageCount = totalPageCount
 
-for i in range(1, totalPageCount, spiderPageCount):
-    if i + spiderPageCount >= totalPageCount:
-        setting.update({
-            'CLOSESPIDER_PAGECOUNT': totalPageCount - i + 1
-        })
+    def spiderProcess(self):
+        process = CrawlerProcess()
+        setting = get_project_settings()
         spiderPageCount = setting.get('CLOSESPIDER_PAGECOUNT')
-        process = CrawlerProcess(setting)
 
-    process.crawl(CrawlSpider, i)
+        for i in range(1, self.totalPageCount, spiderPageCount):
+            if i + spiderPageCount >= self.totalPageCount:
+                setting.update({
+                    'CLOSESPIDER_PAGECOUNT': self.totalPageCount - i + 1
+                })
+                spiderPageCount = setting.get('CLOSESPIDER_PAGECOUNT')
+                process = CrawlerProcess(setting)
 
-process.start()
+            process.crawl(CrawlSpider, i)
+
+        process.start()
